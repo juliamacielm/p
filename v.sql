0054721e2de123693934b664e91e114d26a4384f
Julia Maciel <143205314+juliamacielm@users.noreply.github.com>
2024-09-05 18:26:31 -0300

    Add files via upload

---
+++ v.sql
@@ -0,0 +1,40 @@
+creat database prova22c;
+creat table produtos  (
+idproduto int,
+nomeproduto varchar (255),
+preco int,
+categoria varchar)
+
+in
+
+
+creat table estoque (
+idestoque int,
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
